f8f10458f9966f04a7648a7ac52f9fff3f1533f0
Kyle Buchanan <kylebuch8@gmail.com>
2016-03-28 09:35:57 -0400

    merging

--- app/game/game.component.js
+++ app/game/game.component.js
@@ -20,55 +20,72 @@ System.register(['angular2/core'], function(exports_1, context_1) {
         execute: function() {
             GameComponent = (function () {
                 function GameComponent() {
+                    var _this = this;
                     this.score = 0;
+                    this.teamScore = 0;
+                    this.ws = new WebSocket('ws://localhost:8000/');
+                    this.ws.onopen = function (event) {
+                        console.log(event);
+                    };
+                    this.ws.onmessage = function (event) {
+                        var data = JSON.parse(event.data);
+                        if (data.message === 'team-score') {
+                            _this.teamScore = data.score;
+                        }
+                        if (data.message === 'configuration') {
+                            if (_this.game.stage && data.configuration.backgroundColor) {
+                                _this.game.stage.backgroundColor = data.configuration.backgroundColor;
+                            }
+                        }
+                    };
                 }
                 GameComponent.prototype.ngOnInit = function () {
                     var _this = this;
-                    var game = new Phaser.Game(window.innerWidth, window.innerHeight - 56, Phaser.AUTO, 'game');
+                    this.game = new Phaser.Game(window.innerWidth, window.innerHeight - 56, Phaser.AUTO, 'game');
                     var goodObjects = null;
                     var explosions = null;
                     var fireRate = 100;
                     var nextFire = 0;
                     var TitleState = {
                         update: function () {
-                            var text = game.add.text(Math.floor(game.world.width / 2), Math.floor(game.world.height / 2), 'Tap to Start', {
+                            var text = _this.game.add.text(Math.floor(_this.game.world.width / 2), Math.floor(_this.game.world.height / 2), 'Tap to Start', {
                                 font: '25px Arial',
                                 align: 'center',
                                 fill: '#fff'
                             });
                             text.anchor.setTo(0.5);
-                            game.input.onTap.add(function () {
-                                game.state.start('Ninja');
+                            _this.game.input.onTap.add(function () {
+                                _this.game.state.start('Ninja');
                             });
                         }
                     };
                     var PlayState = {
                         preload: function () {
-                            game.load.image('ball', './app/game/assets/ball.png');
-                            game.load.spritesheet('explosion', './app/game/assets/explosion.png', 128, 128, 10);
+                            _this.game.load.image('ball', './app/game/assets/ball.png');
+                            _this.game.load.spritesheet('explosion', './app/game/assets/explosion.png', 128, 128, 10);
                         },
                         create: function () {
-                            game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-                            game.scale.pageAlignHorizontally = true;
-                            game.scale.pageAlignVertically = true;
-                            game.scale.setScreenSize = true;
-                            game.scale.minWidth = 320;
-                            game.scale.minHeight = 480;
-                            game.scale.forceOrientation(false, true);
-                            game.world.setBounds(0, 0, game.world.width, game.world.height);
-                            game.physics.startSystem(Phaser.Physics.ARCADE);
-                            game.physics.arcade.gravity.y = 300;
+                            _this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+                            _this.game.scale.pageAlignHorizontally = true;
+                            _this.game.scale.pageAlignVertically = true;
+                            _this.game.scale.setScreenSize = true;
+                            _this.game.scale.minWidth = 320;
+                            _this.game.scale.minHeight = 480;
+                            _this.game.scale.forceOrientation(false, true);
+                            _this.game.world.setBounds(0, 0, _this.game.world.width, _this.game.world.height);
+                            _this.game.physics.startSystem(Phaser.Physics.ARCADE);
+                            _this.game.physics.arcade.gravity.y = 300;
                             PlayState.updateNextEvent();
                         },
                         generateBall: function () {
-                            var ball = game.add.sprite(game.rnd.integerInRange(0, game.width), 0, 'ball');
+                            var ball = _this.game.add.sprite(_this.game.rnd.integerInRange(0, _this.game.width), 0, 'ball');
                             ball.scale.setTo(0.4);
                             ball.anchor.setTo(0.5);
                             ball.inputEnabled = true;
                             ball.checkWorldBounds = true;
                             ball.events.onOutOfBounds.add(PlayState.ballOut, _this);
                             ball.events.onInputDown.add(PlayState.handleTap, _this);
-                            game.physics.enable(ball, Phaser.Physics.ARCADE);
+                            _this.game.physics.enable(ball, Phaser.Physics.ARCADE);
                             ball.body.velocity.setTo(0, 200);
                         },
                         ballOut: function (ball) {
@@ -76,34 +93,39 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                         },
                         handleTap: function (ball) {
                             ball.kill();
-                            var explosion = game.add.sprite(ball.body.x, ball.body.y, 'explosion');
+                            var explosion = _this.game.add.sprite(ball.body.x, ball.body.y, 'explosion');
                             explosion.anchor.set(0.25);
                             explosion.animations.add('explode');
                             explosion.animations.play('explode', 30, false, true);
                         },
                         update: function () {
-                            if (game.time.now > game.nextEvent) {
+                            if (_this.game.time.now > _this.game.nextEvent) {
                                 PlayState.updateNextEvent();
                                 PlayState.generateBall();
                             }
                         },
                         updateNextEvent: function () {
-                            game.nextEvent = game.time.now + game.rnd.realInRange(0.5, 1) * Phaser.Timer.SECOND;
+                            _this.game.nextEvent = _this.game.time.now + _this.game.rnd.realInRange(0.5, 1) * Phaser.Timer.SECOND;
                         }
                     };
                     var NinjaState = {
                         preload: function () {
-                            game.time.advancedTiming = true;
-                            game.load.image('ball', './app/game/assets/ball.png');
-                            game.load.spritesheet('explosion', './app/game/assets/explosion.png', 128, 128, 10);
+                            _this.game.time.advancedTiming = true;
+                            _this.game.load.image('ball', './app/game/assets/ball.png');
+                            _this.game.load.image('fruit', './app/game/assets/fruit.png');
+                            _this.game.load.atlas('kitten', './app/game/assets/kitten-sprite.png', './app/game/assets/kitten-sprite.json');
+                            _this.game.load.spritesheet('explosion', './app/game/assets/explosion.png', 128, 128, 10);
                         },
                         create: function () {
-                            game.physics.arcade.gravity.y = 300;
+                            _this.game.physics.arcade.gravity.y = 300;
                             goodObjects = NinjaState.createGroup(4, 'ball');
-                            explosions = game.add.group();
+                            // goodObjects = NinjaState.createGroup(4, 'fruit');
+                            explosions = _this.game.add.group();
                             explosions.createMultiple(4, 'explosion');
                             explosions.children.forEach(function (explosion) {
                                 explosion.anchor.set(-0.125);
+                                // for kittens and fruit
+                                // explosion.anchor.set(0.5);
                                 explosion.animations.add('explode');
                             });
                             NinjaState.throwObject();
@@ -112,18 +134,26 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                             NinjaState.throwObject();
                         },
                         render: function () {
-                            game.debug.text(game.time.fps, 2, 14, "#00ff00");
+                            _this.game.debug.text(_this.game.time.fps, 2, 14, "#00ff00");
                         },
                         createGroup: function (numItems, sprite) {
-                            var group = game.add.group();
+                            var group = _this.game.add.group();
                             group.enableBody = true;
                             group.physicsBodyType = Phaser.Physics.ARCADE;
                             group.createMultiple(numItems, sprite);
+                            // group.createMultiple(numItems, 'kitten');
                             group.setAll('checkWorldBounds', true);
                             group.setAll('outOfBoundsKill', true);
                             group.children.forEach(function (obj) {
-                                obj.anchor.setTo(0.5);
+                                // kittens
+                                // obj.animations.add('forward', Phaser.Animation.generateFrameNames('cat_right', 0, 2, '', 1), 7, true);
+                                // obj.animations.play('forward');
+                                // obj.scale.setTo(1.5);
+                                // fruit
+                                // obj.scale.setTo(1.3);
+                                // basketballs
                                 obj.scale.setTo(0.3);
+                                obj.anchor.setTo(0.5);
                                 obj.inputEnabled = true;
                                 obj.events.onInputDown.add(function () {
                                     obj.kill();
@@ -131,14 +161,17 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                                     var explosion = explosions.getFirstExists(false);
                                     explosion.reset(obj.body.x, obj.body.y);
                                     explosion.play('explode', 30, false, true);
+                                    _this.ws.send(JSON.stringify({
+                                        message: 'score'
+                                    }));
                                 });
                             });
                             return group;
                         },
                         throwObject: function () {
-                            if (game.time.now > nextFire && goodObjects.countDead() > 0) {
+                            if (_this.game.time.now > nextFire && goodObjects.countDead() > 0) {
                                 if (goodObjects.countDead() > 0) {
-                                    nextFire = game.time.now + fireRate;
+                                    nextFire = _this.game.time.now + fireRate;
                                     NinjaState.throwGoodObject();
                                 }
                             }
@@ -149,14 +182,14 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                             game.physics.arcade.moveToXY(obj, game.world.centerX, game.world.centerY, (game.world.height + 56 - 568) * 0.5 + 450);
                         }
                     };
-                    game.state.add('Play', PlayState);
-                    game.state.add('Title', TitleState);
-                    game.state.add('Ninja', NinjaState);
-                    game.state.start('Title');
+                    this.game.state.add('Play', PlayState);
+                    this.game.state.add('Title', TitleState);
+                    this.game.state.add('Ninja', NinjaState);
+                    this.game.state.start('Title');
                 };
                 GameComponent = __decorate([
                     core_1.Component({
-                        template: "<div style=\"position: absolute; z-index: 1; color: white; right: 0;\">{{score}}</div>\n               <div id=\"game\"></div>"
+                        template: "<div style=\"position: absolute; z-index: 1; color: white; right: 0;\">\n                   <p>Team Score: {{teamScore}}</p>\n                   <p>Your Score: {{score}}</p>\n               </div>\n               <div id=\"game\"></div>"
                     }), 
                     __metadata('design:paramtypes', [])
                 ], GameComponent);
--- app/game/game.component.ts
+++ app/game/game.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from 'angular2/core';
 
 @Component({
-    template: `<div style="position: absolute; z-index: 1; color: white; right: 0;">{{score}}</div>
+    template: `<div style="position: absolute; z-index: 1; color: white; right: 0;">
+                   <p>Team Score: {{teamScore}}</p>
+                   <p>Your Score: {{score}}</p>
+               </div>
                <div id="game"></div>`
 })
 
 export class GameComponent implements OnInit {
+    game;
     score = 0;
+    teamScore = 0;
+    ws;
+
+    constructor() {
+        this.ws = new WebSocket('ws://localhost:8000/');
+        this.ws.onopen = event => {
+            console.log(event);
+        };
+
+        this.ws.onmessage = event => {
+            let data = JSON.parse(event.data);
+
+            if (data.message === 'team-score') {
+                this.teamScore = data.score;
+            }
+
+            if (data.message === 'configuration') {
+                if (this.game.stage && data.configuration.backgroundColor) {
+                    this.game.stage.backgroundColor = data.configuration.backgroundColor;
+                }
+            }
+        };
+    }
 
     ngOnInit() {
-        let game = new Phaser.Game(window.innerWidth, window.innerHeight - 56, Phaser.AUTO, 'game');
+        this.game = new Phaser.Game(window.innerWidth, window.innerHeight - 56, Phaser.AUTO, 'game');
         let goodObjects = null;
         let explosions = null;
         let fireRate = 100;
@@ -17,7 +44,7 @@ export class GameComponent implements OnInit {
 
         let TitleState = {
             update: () => {
-                let text = game.add.text(Math.floor(game.world.width / 2), Math.floor(game.world.height / 2), 'Tap to Start', {
+                let text = this.game.add.text(Math.floor(this.game.world.width / 2), Math.floor(this.game.world.height / 2), 'Tap to Start', {
                     font: '25px Arial',
                     align: 'center',
                     fill: '#fff'
@@ -25,34 +52,34 @@ export class GameComponent implements OnInit {
 
                 text.anchor.setTo(0.5);
 
-                game.input.onTap.add(() => {
-                    game.state.start('Ninja')
+                this.game.input.onTap.add(() => {
+                    this.game.state.start('Ninja')
                 });
             }
         };
 
         let PlayState = {
             preload: () => {
-                game.load.image('ball', './app/game/assets/ball.png');
-                game.load.spritesheet('explosion', './app/game/assets/explosion.png', 128, 128, 10);
+                this.game.load.image('ball', './app/game/assets/ball.png');
+                this.game.load.spritesheet('explosion', './app/game/assets/explosion.png', 128, 128, 10);
             },
             create: () => {
-                game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-                game.scale.pageAlignHorizontally = true;
-                game.scale.pageAlignVertically = true;
-                game.scale.setScreenSize = true;
-                game.scale.minWidth = 320;
-                game.scale.minHeight = 480;
-                game.scale.forceOrientation(false, true);
+                this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+                this.game.scale.pageAlignHorizontally = true;
+                this.game.scale.pageAlignVertically = true;
+                this.game.scale.setScreenSize = true;
+                this.game.scale.minWidth = 320;
+                this.game.scale.minHeight = 480;
+                this.game.scale.forceOrientation(false, true);
 
-                game.world.setBounds(0, 0, game.world.width, game.world.height);
-                game.physics.startSystem(Phaser.Physics.ARCADE);
-                game.physics.arcade.gravity.y = 300;
+                this.game.world.setBounds(0, 0, this.game.world.width, this.game.world.height);
+                this.game.physics.startSystem(Phaser.Physics.ARCADE);
+                this.game.physics.arcade.gravity.y = 300;
 
                 PlayState.updateNextEvent();
             },
             generateBall: () => {
-                let ball = game.add.sprite(game.rnd.integerInRange(0, game.width), 0, 'ball');
+                let ball = this.game.add.sprite(this.game.rnd.integerInRange(0, this.game.width), 0, 'ball');
                 ball.scale.setTo(0.4);
                 ball.anchor.setTo(0.5);
 
@@ -61,7 +88,7 @@ export class GameComponent implements OnInit {
                 ball.events.onOutOfBounds.add(PlayState.ballOut, this);
                 ball.events.onInputDown.add(PlayState.handleTap, this);
 
-                game.physics.enable(ball, Phaser.Physics.ARCADE);
+                this.game.physics.enable(ball, Phaser.Physics.ARCADE);
                 ball.body.velocity.setTo(0, 200);
             },
             ballOut: ball => {
@@ -70,36 +97,41 @@ export class GameComponent implements OnInit {
             handleTap: ball => {
                 ball.kill();
 
-                let explosion = game.add.sprite(ball.body.x, ball.body.y, 'explosion');
+                let explosion = this.game.add.sprite(ball.body.x, ball.body.y, 'explosion');
                 explosion.anchor.set(0.25);
                 explosion.animations.add('explode');
                 explosion.animations.play('explode', 30, false, true);
             },
             update: () => {
-                if (game.time.now > game.nextEvent) {
+                if (this.game.time.now > this.game.nextEvent) {
                     PlayState.updateNextEvent();
                     PlayState.generateBall();
                 }
             },
             updateNextEvent: () => {
-                game.nextEvent = game.time.now + game.rnd.realInRange(0.5, 1) * Phaser.Timer.SECOND;
+                this.game.nextEvent = this.game.time.now + this.game.rnd.realInRange(0.5, 1) * Phaser.Timer.SECOND;
             }
         };
 
         let NinjaState = {
             preload: () => {
-                game.time.advancedTiming = true;
-                game.load.image('ball', './app/game/assets/ball.png');
-                game.load.spritesheet('explosion', './app/game/assets/explosion.png', 128, 128, 10);
+                this.game.time.advancedTiming = true;
+                this.game.load.image('ball', './app/game/assets/ball.png');
+                this.game.load.image('fruit', './app/game/assets/fruit.png');
+                this.game.load.atlas('kitten', './app/game/assets/kitten-sprite.png', './app/game/assets/kitten-sprite.json');
+                this.game.load.spritesheet('explosion', './app/game/assets/explosion.png', 128, 128, 10);
             },
             create: () => {
-                game.physics.arcade.gravity.y = 300;
+                this.game.physics.arcade.gravity.y = 300;
                 goodObjects = NinjaState.createGroup(4, 'ball');
+                // goodObjects = NinjaState.createGroup(4, 'fruit');
 
-                explosions = game.add.group();
+                explosions = this.game.add.group();
                 explosions.createMultiple(4, 'explosion');
                 explosions.children.forEach(explosion => {
                     explosion.anchor.set(-0.125);
+                    // for kittens and fruit
+                    // explosion.anchor.set(0.5);
                     explosion.animations.add('explode');
                 });
 
@@ -109,19 +141,30 @@ export class GameComponent implements OnInit {
                 NinjaState.throwObject();
             },
             render: () => {
-                game.debug.text(game.time.fps, 2, 14, "#00ff00");
+                this.game.debug.text(this.game.time.fps, 2, 14, "#00ff00");
             },
             createGroup: (numItems, sprite) => {
-                let group = game.add.group();
+                let group = this.game.add.group();
                 group.enableBody = true;
                 group.physicsBodyType = Phaser.Physics.ARCADE;
                 group.createMultiple(numItems, sprite);
+                // group.createMultiple(numItems, 'kitten');
                 group.setAll('checkWorldBounds', true);
                 group.setAll('outOfBoundsKill', true);
 
                 group.children.forEach(obj => {
-                    obj.anchor.setTo(0.5);
+                    // kittens
+                    // obj.animations.add('forward', Phaser.Animation.generateFrameNames('cat_right', 0, 2, '', 1), 7, true);
+                    // obj.animations.play('forward');
+                    // obj.scale.setTo(1.5);
+
+                    // fruit
+                    // obj.scale.setTo(1.3);
+
+                    // basketballs
                     obj.scale.setTo(0.3);
+
+                    obj.anchor.setTo(0.5);
                     obj.inputEnabled = true;
                     obj.events.onInputDown.add(() => {
                         obj.kill();
@@ -131,29 +174,38 @@ export class GameComponent implements OnInit {
                         var explosion = explosions.getFirstExists(false);
                         explosion.reset(obj.body.x, obj.body.y);
                         explosion.play('explode', 30, false, true);
+
+                        this.ws.send(JSON.stringify({
+                            message: 'score'
+                        }));
                     });
                 });
 
                 return group;
             },
             throwObject: () => {
-                if (game.time.now > nextFire && goodObjects.countDead() > 0) {
+                if (this.game.time.now > nextFire && goodObjects.countDead() > 0) {
                     if (goodObjects.countDead() > 0) {
-                        nextFire = game.time.now + fireRate;
+                        nextFire = this.game.time.now + fireRate;
                         NinjaState.throwGoodObject();
                     }
                 }
             },
             throwGoodObject: () => {
                 var obj = goodObjects.getFirstDead();
+<<<<<<< 454a6a0725c637add0a0060b67e2b8b5dbace798
                 obj.reset(game.world.centerX + Math.random() * 100 - Math.random() * 100, game.world.height);
                 game.physics.arcade.moveToXY(obj, game.world.centerX, game.world.centerY, (game.world.height + 56 - 568) * 0.5 + 450);
+=======
+                obj.reset(this.game.world.centerX + Math.random() * 100 - Math.random() * 100, this.game.world.height);
+                this.game.physics.arcade.moveToXY(obj, this.game.world.centerX, this.game.world.centerY, this.game.world.height - 100);
+>>>>>>> added web sockets and configuration change listener
             }
         };
 
-        game.state.add('Play', PlayState);
-        game.state.add('Title', TitleState);
-        game.state.add('Ninja', NinjaState);
-        game.state.start('Title');
+        this.game.state.add('Play', PlayState);
+        this.game.state.add('Title', TitleState);
+        this.game.state.add('Ninja', NinjaState);
+        this.game.state.start('Title');
     }
 }
