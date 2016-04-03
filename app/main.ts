3cd0a87fb6c6f2d584ee0a331063808d4a9d9527
Kyle Buchanan <kylebuch8@gmail.com>
2016-04-03 11:18:09 -0400

    added ability to upload selfie

--- app/main.ts
+++ app/main.ts
@@ -2,6 +2,8 @@ import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from './app-component/app.component';
 import {ROUTER_PROVIDERS} from 'angular2/router';
 
+import 'rxjs/Rx';
+
 bootstrap(AppComponent, [
     ROUTER_PROVIDERS
 ]);
