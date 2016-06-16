d76c2ad86b2cb0ab30bc24d28295c28819860f13
Kyle Buchanan <kylebuch8@gmail.com>
2016-06-16 16:05:46 -0400

    updated achievements on the leaderboard

--- src/app/+leaderboard/leaderboard.component.ts
+++ src/app/+leaderboard/leaderboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
+import { GameService } from '../+game';
 
 import 'rxjs/add/operator/toPromise';
 
@@ -11,107 +12,57 @@ import 'rxjs/add/operator/toPromise';
 })
 export class LeaderboardComponent {
   error:Boolean = false;
-  playerId: String;
-  achievementsUrl: String = 'http://achievement-server-demo.apps.demo.aws.paas.ninja/api/achievement';
-  achievements: any[];
-  playerAchievements: any[];
   achievementIcons: string[] = ['sentiment_very_satisfied', 'local_play', 'whatshot', 'local_florist', 'local_pizza'];
-  topScores: any[] = [
-    {
-      "name": "Kyle",
-      "points": "100"
-    },
-    {
-      "name": "Joshua",
-      "points": "90"
-    },
-    {
-      "name": "Andres",
-      "points": "85"
-    },
-    {
-      "name": "Michael",
-      "points": "80"
-    },
-    {
-      "name": "Brian",
-      "points": "75"
-    },
-    {
-      "name": "Sally",
-      "points": "70"
-    },
-    {
-      "name": "Mark",
-      "points": "65"
-    },
-    {
-      "name": "Funny Face",
-      "points": "60"
-    },
-    {
-      "name": "Bugs Bunny",
-      "points": "55"
-    },
-    {
-      "name": "Mega Man",
-      "points": "50"
-    },
-    {
-      "name": "Professor X",
-      "points": "45"
-    }
-  ];
 
-  constructor(private http:Http) {
-    let promises = [];
-
-    promises.push(this.getAchievements());
-
-    this.playerId = localStorage.getItem('player-id');
-
-    if (this.playerId) {
-      promises.push(this.getPlayerAchievements(this.playerId));
-    }
-
-    Promise.all(promises)
-      .then(responses => {
-        /*
-         * responses will either have 1 or 2 items in the array
-         *
-         * we'll get two responses if we have a player id in localstorage
-         * when the page loads
-         *
-         * responses[0] will be all of the achievements
-         * if there is a player id, responses[1] will be the player's achievements
-         */
-        this.achievements = responses[0];
-
-        if (responses[1]) {
-          this.playerAchievements = responses[1];
-          this.achievements.forEach(achievement => {
-            this.playerAchievements.forEach(playerAchievement => {
-              if (achievement.achievementType === playerAchievement.achievementType) {
-                achievement.achieved = playerAchievement.achieved;
-              }
-            });
-          });
-        }
-      }, () => {
-        console.log('there was an error');
-      });
+  constructor(private http:Http, private gameService: GameService) {
+    // let promises = [];
+    //
+    // promises.push(this.getAchievements());
+    //
+    // this.playerId = localStorage.getItem('player-id');
+    //
+    // if (this.playerId) {
+    //   promises.push(this.getPlayerAchievements(this.playerId));
+    // }
+    //
+    // Promise.all(promises)
+    //   .then(responses => {
+    //     /*
+    //      * responses will either have 1 or 2 items in the array
+    //      *
+    //      * we'll get two responses if we have a player id in localstorage
+    //      * when the page loads
+    //      *
+    //      * responses[0] will be all of the achievements
+    //      * if there is a player id, responses[1] will be the player's achievements
+    //      */
+    //     this.achievements = responses[0];
+    //
+    //     if (responses[1]) {
+    //       this.playerAchievements = responses[1];
+    //       this.achievements.forEach(achievement => {
+    //         this.playerAchievements.forEach(playerAchievement => {
+    //           if (achievement.achievementType === playerAchievement.achievementType) {
+    //             achievement.achieved = playerAchievement.achieved;
+    //           }
+    //         });
+    //       });
+    //     }
+    //   }, () => {
+    //     console.log('there was an error');
+    //   });
   }
 
-  getAchievements() {
-    return this.http.get(`${this.achievementsUrl}`)
-      .toPromise()
-      .then(response => response.json());
-  }
-
-  getPlayerAchievements(playerId:String) {
-    return this.http.get(`${this.achievementsUrl}/${playerId}`)
-      .toPromise()
-      .then(response => response.json());
-  }
+  // getAchievements() {
+  //   return this.http.get(`${this.achievementsUrl}`)
+  //     .toPromise()
+  //     .then(response => response.json());
+  // }
+  //
+  // getPlayerAchievements(playerId:String) {
+  //   return this.http.get(`${this.achievementsUrl}/${playerId}`)
+  //     .toPromise()
+  //     .then(response => response.json());
+  // }
 
 }
